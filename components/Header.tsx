b3f559183feee5af5be5cc935e64a808cb248b60
juleni <69912179+juleni@users.noreply.github.com>
2023-09-19 12:25:31 +0200

    Added Header component containing Social icons + motion

---
+++ components/Header.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import { motion } from "framer-motion";
+import { SocialIcon } from "react-social-icons";
+
+type Props = {};
+
+function Header({}: Props) {
+  return (
+    <header className="sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center">
+      <motion.div
+        initial={{ x: -500, opacity: 0, scale: 0.5 }}
+        animate={{ x: 0, opacity: 1, scale: 1 }}
+        transition={{ duration: 1.5 }}
+        className="flex flex-row items-center"
+      >
+        <SocialIcon
+          target="_blank"
+          url="https://github.com/juleni"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+        <SocialIcon
+          target="_blank"
+          url="https://sk.linkedin.com/in/julian-legeny-45259b208"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+        <SocialIcon
+          target="_blank"
+          url="https://instagram.com"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+        <SocialIcon
+          target="_blank"
+          href="https://juleni.github.io/blog/"
+          url="https://blog.com"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+      </motion.div>
+      <motion.div
+        initial={{ x: 500, opacity: 0, scale: 0.5 }}
+        animate={{ x: 0, opacity: 1, scale: 1 }}
+        transition={{ duration: 1 }}
+        className="flex flex-row items-center text-gray-300 cursor-pointer"
+      >
+        <SocialIcon
+          className="cursor-pointer"
+          network="email"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+        <p className="uppercase hidden md:inline-flex text-sm text-gray-400">
+          Get In Touch
+        </p>
+      </motion.div>
+    </header>
+  );
+}
+
+export default Header;
