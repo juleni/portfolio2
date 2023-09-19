806291f9b1e18ebf913f0ca59d00fe5774f9b68a
juleni <69912179+juleni@users.noreply.github.com>
2023-09-19 15:51:13 +0200

    Added Hero component and styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,17 @@
 import Header from "@/components/Header";
+import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
     <main>
-      <div>
+      <div className="bg-[rgb(36,36,36)] text-[rgb(220,220,220)] h-screen snap-y snap-mandatory overflow-scroll z-0">
         {/** Header */}
         <Header />
 
         {/** Hero */}
+        <section id="hero" className="snap-center">
+          <Hero />
+        </section>
 
         {/** About */}
 
