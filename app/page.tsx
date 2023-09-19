e624827d0d898baf040385a79ef63833d886fd51
juleni <69912179+juleni@users.noreply.github.com>
2023-09-19 12:23:29 +0200

    Added page skeleton + Header

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,24 @@
+import Header from "@/components/Header";
+
 export default function Home() {
   return (
     <main>
-      <div>Portfolio</div>
+      <div>
+        {/** Header */}
+        <Header />
+
+        {/** Hero */}
+
+        {/** About */}
+
+        {/** Experience */}
+
+        {/** Skills */}
+
+        {/** Projects */}
+
+        {/** Contact Me */}
+      </div>
     </main>
   );
 }
