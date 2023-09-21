3191623a434b356bb29f2d83e5f38663e73eefc3
juleni <69912179+juleni@users.noreply.github.com>
2023-09-21 13:32:06 +0200

    fixed overflow style ... hidden x-scrollbar

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,10 @@ import WorkExperiences from "@/components/WorkExperiences";
 export default function Home() {
   return (
     <main>
-      <div className="bg-[rgb(36,36,36)] text-[rgb(220,220,220)] h-screen snap-y snap-mandatory overflow-scroll z-0">
+      <div
+        className="bg-[rgb(36,36,36)] text-[rgb(220,220,220)] h-screen snap-y snap-mandatory 
+                  overflow-y-scroll overflow-x-hidden z-0"
+      >
         <Header />
 
         <section id="hero" className="snap-start">
