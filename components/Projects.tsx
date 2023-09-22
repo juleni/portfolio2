c75cf00b8f9d159d2514473f15dd7b7bfac6e04e
juleni <69912179+juleni@users.noreply.github.com>
2023-09-23 00:00:14 +0200

    Added ProjectCard component

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,9 +1,40 @@
 "use client";
 import { motion } from "framer-motion";
+import ProjectCard from "./ProjectCard";
 type Props = {};
 
+type ProjectType = {
+  id: number;
+  img: string;
+  url: string;
+  name: string;
+  desc: string;
+};
+
+const projects = [
+  {
+    id: 1,
+    img: "images/projects/ai-image-generator.jpg",
+    url: "https://ai-image-generator-brown.vercel.app/",
+    name: "AI Image Generator",
+    desc: "AI Image generator uses OpenAI API of trained neural network DALL-E to create images from text captions.",
+  },
+  {
+    id: 2,
+    img: "images/projects/trello-clone.jpg",
+    url: "https://trello-clone-juleni.vercel.app/",
+    name: "GPT Trello Clone",
+    desc: "Chat GPT Trello Clone allows to create, move, delete, search tasks and summarising them.",
+  },
+  {
+    id: 3,
+    img: "images/projects/sorting-hat.jpg",
+    url: "https://ai-sorting-hat.vercel.app/",
+    name: "AI Sorting Hat",
+    desc: "The sorting hat determines based on your answer to which of the four school Houses you belong most to. These four Houses are Gryffindor, Hufflepuff, Ravenclaw, and Slytherin.",
+  },
+];
 function Projects({}: Props) {
-  const projects = [1, 2, 3, 4, 5];
   return (
     <motion.div
       initial={{ opacity: 0 }}
@@ -17,38 +48,13 @@ function Projects({}: Props) {
       </h3>
       <div
         className="relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20
-                   scrollbar scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-[#f7ab0a]/60"
+               scrollbar scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-[#f7ab0a]/60"
       >
-        {projects.map((project, i) => (
-          <div
-            className="h-screen w-screen flex flex-col flex-shrink-0 snap-center space-y-5 items-center 
-                       justify-center p-20 md:p-44"
-            key={i}
-          >
-            <motion.img
-              initial={{ y: -300, opacity: 0 }}
-              transition={{ duration: 1.2 }}
-              whileInView={{ y: 0, opacity: 1 }}
-              viewport={{ once: true }}
-              src="apiprodukt.png"
-              alt=""
-            />
-            <div className="space-y-10 px-0 md:px-10 max-w-6xl">
-              <h4 className="text-2xl font-semibold text-center">
-                <span className="underline decoration-[#f7ab0a]/50">
-                  Personal project {i + 1}:
-                </span>{" "}
-                AI Image Generator
-              </h4>
-              <p className="text-lg text-justify">
-                AI Image generator uses OpenAI API of trained neural network
-                DALL-E to create images from text captions.
-              </p>
-            </div>
-          </div>
-        ))}
+        <ProjectCard {...projects[0]} />
+        <ProjectCard {...projects[1]} />
+        <ProjectCard {...projects[2]} />
       </div>
-      <div className="w-full absolute top-[30%] bg-[#f7ab0a]/10 left-0 h-[500px] -skew-y-12" />
+      <div className="w-full absolute top-[30%] bg-[#f7ab0a]/10 left-0 h-[500px] -skew-y-12 z-1000" />
     </motion.div>
   );
 }
