89f5c4da98af853de94e10400cfbab65c30d63f0
juleni <69912179+juleni@users.noreply.github.com>
2023-09-22 21:37:27 +0200

    Changed styles

--- components/About.tsx
+++ components/About.tsx
@@ -9,10 +9,10 @@ function About({}: Props) {
       initial={{ opacity: 0 }}
       transition={{ duration: 1.5 }}
       whileInView={{ opacity: 1 }}
-      className="flex flex-col md:flex-row relative h-screen text-center md:text-left 
-                 max-w-7xl px-10 justify-evenly mx-auto items-center"
+      className="flex flex-col md:flex-row relative h-screen text-left max-w-full 
+      px-10 justify-evenly mx-auto items-center overflow-hidden"
     >
-      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
+      <h3 className="absolute top-20 uppercase tracking-[20px] text-gray-500 text-2xl">
         About Me
       </h3>
       <motion.img
@@ -20,8 +20,8 @@ function About({}: Props) {
         transition={{ duration: 1.2 }}
         whileInView={{ x: 0, opacity: 1 }}
         viewport={{ once: true }}
-        src="pic-about.jpg"
-        className="-mb-24 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg 
+        src="images/pic-about.jpg"
+        className="mt-5 -mb-24 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg 
                    md:w-64 md:h-95 xl:w-[480px] xl:h-[640px]"
       ></motion.img>
       <div className="space-y-5 px-0 md:px-10 mt-5">
@@ -34,7 +34,7 @@ function About({}: Props) {
   */}
         <div
           className="h-[350px] md:h-[600px] xl:h-full overflow-y-auto scrollbar
-                      scrollbar-thin  scrollbar-track-gray-400/20 scrollbar-thumb-[#f7ab0a]/60"
+                     scrollbar-thin  scrollbar-track-gray-400/20 scrollbar-thumb-[#f7ab0a]/60"
         >
           <p className="text-base">
             Ever since I was a child, during my studies at primary school (90s
