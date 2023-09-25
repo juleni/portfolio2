4ab11729560b1c8b1397f4d387788d7fbca19c52
juleni <69912179+juleni@users.noreply.github.com>
2023-09-25 15:01:30 +0200

    Commented distDir

--- next.config.js
+++ next.config.js
@@ -15,7 +15,7 @@ const nextConfig = {
   // skipTrailingSlashRedirect: true,
 
   // Optional: Change the output directory `out` -> `dist`
-  distDir: "out",
+  // distDir: "out",
   images: {
     unoptimized: true,
   },
