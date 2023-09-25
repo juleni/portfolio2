c099fa47b5b97193d11752605ac2606c6eac894e
juleni <69912179+juleni@users.noreply.github.com>
2023-09-25 14:58:54 +0200

    Changed settings

--- next.config.js
+++ next.config.js
@@ -1,4 +1,24 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+/**
+ * @type {import('next').NextConfig}
+ */
 
-module.exports = nextConfig
+const isProd = process.env.NODE_ENV === "production";
+
+const nextConfig = {
+  basePath: isProd ? "/portfolio2" : "",
+  output: "export",
+
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+
+  // Optional: Change the output directory `out` -> `dist`
+  distDir: "dist",
+  images: {
+    unoptimized: true,
+  },
+};
+
+module.exports = nextConfig;
